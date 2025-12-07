61080f4010bbbd660b0f4fceaac6f115655b8c62
Clemv95 <32482023+Clemv95@users.noreply.github.com>
2025-12-07 15:49:23 +0000

    Update init

--- init-indexers.sh
+++ init-indexers.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir -p /app/Definitions/Custom/
+mkdir -p /app/Definitions/
 
 # Copy YGG-API indexer
 echo "Installing YGG-API indexer definition..."
-cp /app/indexer-definitions/ygg-api-download.yml /app/Definitions/Custom/
+cp /app/indexer-definitions/ygg-api-download.yml /app/Definitions/
 
 # Copy ygege indexer
 echo "Installing ygege indexer definition..."
-cp /app/indexer-definitions/ygege.yml /app/Definitions/Custom/
+cp /app/indexer-definitions/ygege.yml /app/Definitions/
 
 chown -R hotio:hotio /app/Definitions/
 chmod -R 755 /app/Definitions/
