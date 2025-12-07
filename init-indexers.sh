4456835ecaca2944d1b9d69b343bd2e7ee363e1d
Clemv95 <32482023+Clemv95@users.noreply.github.com>
2025-12-07 15:44:10 +0000

    Update init-indexers

--- init-indexers.sh
+++ init-indexers.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+mkdir -p /app/Definitions/Custom/
+
 # Copy YGG-API indexer
 echo "Installing YGG-API indexer definition..."
-cp /app/indexer-definitions/ygg-api-download.yml /app/Definitions/
+cp /app/indexer-definitions/ygg-api-download.yml /app/Definitions/Custom/
 
 # Copy ygege indexer
 echo "Installing ygege indexer definition..."
-cp /app/indexer-definitions/ygege.yml /app/Definitions/
+cp /app/indexer-definitions/ygege.yml /app/Definitions/Custom/
+
+chown -R hotio:hotio /app/Definitions/
+chmod -R 755 /app/Definitions/
 
 echo "Indexers installed successfully (YGG-API, ygege)"
 
